56ccae52e24d3c58722428d61dc41aa82081e425
Kiruthika <kiruthikamuthukumar2004@gmail.com>
2026-07-19 09:18:29 +0530

    update gemini model

--- gemini_service.py
+++ gemini_service.py
@@ -8,7 +8,7 @@ from langchain_core.messages import HumanMessage
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash-lite",
+    model="gemini-2.5-flash-preview-05-20",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
     temperature=0.3
 )
